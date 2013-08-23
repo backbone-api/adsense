d8605c4b02bd4cea1b79b43ce447cf2969a2c505
Makis Tracend <makis.tracend@gmail.com>
2013-08-23 01:19:44 -0700

    syntax fixes

--- backbone.api.adsense.js
+++ backbone.api.adsense.js
@@ -2,7 +2,7 @@
 
 	(adsbygoogle = window.adsbygoogle || []).push({});
 	// load dependencies (assume jQuery is available?)
-	var async = ( c && !_.isUndefined(c.script) ) ? c.sript : $.getScript;
+	var async = ( typeof c != "undefined" && !_.isUndefined(c.script) ) ? c.sript : $.getScript;
 	async("http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js")
 
 	var Adsense = new Backbone.Model({
@@ -19,21 +19,14 @@
 			height: 90
 		},
 		render: function(){
-			var html = template( this.options ) );
+			var html = template( this.options );
 			$(this.el).html( html );
 		}
 	});
 
 	// Templates
 	// support mustache syntax
-
 	_.templateSettings.interpolate = /\{\{(.+?)\}\}/g;
-
 	var template = _.template('<ins class="adsbygoogle" style="display:inline-block;width:{{width}}px;height:{{height}}px" data-ad-client="ca-{{pubID}}" data-ad-slot="{{id}}"></ins>');
-
-<script>
-
-
-</script>
 
 })(this.Backbone, this._, this.$);
