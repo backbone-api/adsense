455ff42631ab3fa4b3c8e9c450df6bda62c80b55
Makis Tracend <makis.tracend@gmail.com>
2013-08-23 01:59:02 -0700

    tweaks to normalize behaviour

--- backbone.api.adsense.js
+++ backbone.api.adsense.js
@@ -1,12 +1,12 @@
 (function(Backbone, _, $){
 
-	(adsbygoogle = window.adsbygoogle || []).push({});
+	window.adsbygoogle = window.adsbygoogle || [];
 	// load dependencies (assume jQuery is available?)
 	var async = ( typeof c != "undefined" && !_.isUndefined(c.script) ) ? c.sript : $.getScript;
 	async("http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js")
 
 	var Adsense = new Backbone.Model({
-		pubID: 0
+		id: 0
 	});
 
 	// namespace
@@ -14,13 +14,21 @@
 	Adsense.Views = {};
 
 	Adsense.Views.Banner = Backbone.View.extend({
+		el: ".banner",
 		options: {
 			width: 728,
 			height: 90
 		},
+		initialize: function( options ){
+			this.options.pubID = Adsense.get("id");
+			this.render();
+			return Backbone.View.prototype.initialize.call( this, options );
+		},
 		render: function(){
 			var html = template( this.options );
 			$(this.el).html( html );
+			// queue an ad
+			window.adsbygoogle.push({});
 		}
 	});
 
@@ -28,5 +36,7 @@
 	// support mustache syntax
 	_.templateSettings.interpolate = /\{\{(.+?)\}\}/g;
 	var template = _.template('<ins class="adsbygoogle" style="display:inline-block;width:{{width}}px;height:{{height}}px" data-ad-client="ca-{{pubID}}" data-ad-slot="{{id}}"></ins>');
+
+	if(typeof window.Adsense == "undefined") window.Adsense = Adsense;
 
 })(this.Backbone, this._, this.$);
